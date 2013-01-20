412bbf082818bb464cd744aba039afd1ce9ae489
Jason Kinner <jkinner@sociodyne.com>
2013-01-20 15:02:09 -0500

    Flag sample

---
+++ sample/hello_flag.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag";
+	"fmt";
+	"github.com/jkinner/goose";
+	"reflect";
+)
+
+var name *string = flag.String("name", "world", "whom to say hello to")
+
+func main() {
+	flag.Parse()
+	injector := goose.CreateInjector()
+	injector.Bind(reflect.TypeOf(""), func () (interface{}) { return *name })
+	sayHello(injector)
+}
+
+func sayHello(injector goose.Injector) {
+	fmt.Println("Hello", injector.GetInstance(reflect.TypeOf("")), "!")
+}
